58d6bda098797c8a7d4af8efdd3ad583d945f73a
athrun127226 <athrun127226@gmail.com>
2026-04-11 21:54:08 +0800

    fix: 在 main.tsx 中导入 i18n 初始化国际化

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './i18n' // 初始化国际化
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
