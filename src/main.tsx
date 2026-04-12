726934ee812c390222d474f7bf7acc4bed5eeba7
athrun127226 <athrun127226@gmail.com>
2026-04-12 19:59:17 +0800

    feat: 集成 Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './i18n' // 初始化国际化
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
